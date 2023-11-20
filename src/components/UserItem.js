79308d223b6a7cdc50b78faec9e0b62ad55d34c9
firas hamila <firas.hm.1996@gmail.com>
2023-11-20 18:45:36 +0100

    sss

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,13 +1,30 @@
 import { Text, StyleSheet, View, Pressable } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
+import { useContext, useState } from "react";
+import { FavUsersContext } from "../store/context/favUsersContext";
 
 const UserItem = (props) => {
+  const [isFav, setIsFav] = useState(false);
+  const ctx = useContext(FavUsersContext);
+  const favHandler = () => {
+    setIsFav(!isFav);
+    if (isFav) {
+      ctx.removeFavorite(props.id);
+    } else {
+      ctx.addFavorite(props.id);
+    }
+    console.log(ctx.favUsersIds);
+  };
   return (
     <View style={styles.userItem}>
       <Text style={styles.userText}>{props.name}</Text>
-      <Pressable onPress={props.removeUser.bind(this, props.id)}>
-        <Ionicons name="star-outline" size={24} color="white" />
+      <Pressable onPress={favHandler}>
+        <Ionicons
+          name={isFav ? "star" : "star-outline"}
+          size={24}
+          color="white"
+        />
       </Pressable>
     </View>
   );
